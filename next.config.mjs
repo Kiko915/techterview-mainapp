4302c3b4e80fe61e1987879ec03a74b6eac3a18d
kikodevelops <kikoneilmistica@gmail.com>
2025-10-17 20:30:17 +0800

    fix: configure Next.js to allow Firebase Storage images…

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/v0/b/techterview-webapp.firebasestorage.app/**',
+      },
+      // You can add other domains here if needed
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
